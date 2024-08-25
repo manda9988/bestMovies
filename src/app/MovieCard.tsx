f5b32aae7fe0bf64d9d0a2cc60cb3be76d43ae67
KAEL <vincent.achy@gmail.com>
2024-08-25 19:26:05 +0200

    commit

--- src/app/MovieCard.tsx
+++ src/app/MovieCard.tsx
@@ -13,8 +13,7 @@ interface Movie {
 
 export default function MovieCard({ movie }: { movie: Movie }) {
   return (
-    <div className="flex bg-gray-200 shadow-md rounded-lg overflow-hidden">
-      {/* Utilisation du ratio 3:4 pour l'image */}
+    <div className="flex bg-gray-900 shadow-md rounded-lg overflow-hidden">
       <div className="w-[35%] rounded-md overflow-hidden relative">
         <img
           src={movie.imageUrl}
@@ -24,18 +23,18 @@ export default function MovieCard({ movie }: { movie: Movie }) {
         <div className="pt-[133.33%]"></div> {/* Maintien du ratio 3:4 */}
       </div>
 
-      <div className="w-[65%] p-4 pt-1">
-        <h2 className="text-2xl font-bold mb-2">{movie.title}</h2>
-        <p className="text-gray-500 mb-2 font-normal">
+      <div className="w-[65%] p-4 pt-2">
+        <h2 className="text-2xl font-bold mb-2 text-gray-100">{movie.title}</h2>
+        <p className="text-gray-400 mb-2 font-normal">
           {movie.releaseDate} |{" "}
-          <span className=" text-gray-800 font-semibold">{movie.genre}</span>
+          <span className="text-gray-200 font-semibold">{movie.genre}</span>
         </p>
-        <p className="text-gray-500 mb-2 font-normal">
+        <p className="text-gray-400 mb-2 font-normal">
           De{" "}
-          <span className="text-gray-800 font-semibold">{movie.director}</span>
+          <span className="text-gray-200 font-semibold">{movie.director}</span>
         </p>
-        <p className="text-gray-500 mb-2 font-normal">
-          Avec <span className="text-gray-800 font-semibold">{movie.cast}</span>
+        <p className="text-gray-400 mb-2 font-normal">
+          Avec <span className="text-gray-200 font-semibold">{movie.cast}</span>
         </p>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,16 +16,12 @@ export default function Layout({ children }: LayoutProps) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body className="flex flex-col min-h-screen">
-        <header className="bg-gray-800 text-white py-4">
-          <div className="container mx-auto px-4">
-            <h1 className="text-2xl font-bold">BestMovies</h1>
-          </div>
+        <header>
+          <h1 className="text-2xl font-bold">BestMovies</h1>
         </header>
-        <main className="flex-grow">{children}</main>
-        <footer className="bg-gray-800 text-white py-4">
-          <div className="container mx-auto px-4">
-            <p>&copy; 2024 BestMovies. Tous droits réservés.</p>
-          </div>
+        <main>{children}</main>
+        <footer>
+          <p>&copy; 2024 BestMovies. Tous droits réservés.</p>
         </footer>
       </body>
     </html>
